ee77dcf39553b5a677ed5d7cd138947918a57877
Claudio Delkahez <cdelkahez.dev@gmail.com>
2022-04-08 20:26:01 -0300

    shopping cart finished

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
 let navbar = document.querySelector('.header .flex .navbar')
+let myOrders = document.querySelector('.my-orders')
+let account = document.querySelector('.user-account')
+let cart = document.querySelector('.shopping-cart')
 
+
+//navbar
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
 };
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    cart.classList.remove('active');
+    myOrders.classList.remove('active');
 
-let account = document.querySelector('.user-account')
-
+};
+// Account
 document.querySelector('#user-btn').onclick = () => {
     account.classList.add('active');
 };
 document.querySelector('#close-account').onclick = () => {
     account.classList.remove('active');
 };
-
-let myOrders = document.querySelector('.my-orders')
-
+// My orders
 document.querySelector('#order-btn').onclick = () => {
     myOrders.classList.add('active');
 };
@@ -22,7 +29,13 @@ document.querySelector('#close-orders').onclick = () => {
     myOrders.classList.remove('active');
 };
 
-
-window.onscroll = () => {
-    navbar.classList.remove('active');
+// cart
+document.querySelector('#cart-btn').onclick = () => {
+    cart.classList.add('active');
 };
+document.querySelector('#close-cart').onclick = () => {
+    cart.classList.remove('active');
+};
+
+
+
