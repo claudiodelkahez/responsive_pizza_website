14cc5291839d4cf9678b2b70927402ad5204135b
Claudio Delkahez <cdelkahez.dev@gmail.com>
2022-04-08 17:35:47 -0300

    Account completed

--- js/script.js
+++ js/script.js
@@ -3,6 +3,17 @@ let navbar = document.querySelector('.header .flex .navbar')
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
 };
+
+let account = document.querySelector('.user-account')
+
+document.querySelector('#user-btn').onclick = () => {
+    account.classList.add('active');
+};
+document.querySelector('#close-account').onclick = () => {
+    account.classList.remove('active');
+};
+
+
 window.onscroll = () => {
     navbar.classList.remove('active');
 };
